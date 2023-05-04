8a95c6fb73c53c007ca561b588b15d65b4fe9d87
gaetan <gaetan.gerard27@gmail.com>
2023-05-04 09:42:09 +0200

    fix responsive for slider navigation button

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -60,19 +60,21 @@ const Slider: React.FC<ISliderProps> = ({ items, sliderTitle, position }) => {
     return (
         <div className={`slider-container slider-position-${position} ${/Mobi/.test(navigator.userAgent) ? "slider-mobile" : ""}`}>
             <Typography HTMLElement="h2" classname="slider-title">{sliderTitle}</Typography>
-            <div className="slider-wrapper" style={{ transform: `translateX(-${currentIndex * 225}px)` }}>
-                {newList.map((item, index=1) => (
-                <div key={index} className="slider-item">
-                    <ItemCard key={item.id} item={item} itemID={index++} isInMyList={itemInMyList(p.profile.my_list, item)} />
+            <div className="slider-wrapper-container">
+                <div className="slider-wrapper" style={{ transform: `translateX(-${currentIndex * 225}px)` }}>
+                    {newList.map((item, index=1) => (
+                    <div key={index} className="slider-item">
+                        <ItemCard key={item.id} item={item} itemID={index++} isInMyList={itemInMyList(p.profile.my_list, item)} />
+                    </div>
+                    ))}
                 </div>
-                ))}
+                <button className="slider-button slider-button-prev" onClick={handlePrevClick}>
+                    <Chevron classname="icon" />
+                </button>
+                <button className="slider-button slider-button-next" onClick={handleNextClick}>
+                    <Chevron classname="icon" />
+                </button>
             </div>
-            <button className="slider-button slider-button-prev" onClick={handlePrevClick}>
-                <Chevron classname="icon" />
-            </button>
-            <button className="slider-button slider-button-next" onClick={handleNextClick}>
-                <Chevron classname="icon" />
-            </button>
         </div>
     );
 };
